07ac0e2c456cc337c6ea2fca0e319c59a2f55782
topjhoh20 <topjhoh20@naver.com>
2023-07-17 15:57:29 +0900

    230717 1557

--- TeamProject/src/main/java/com/tp/TeamProjectApplication.java
+++ TeamProject/src/main/java/com/tp/TeamProjectApplication.java
@@ -2,18 +2,11 @@ package com.tp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class TeamProjectApplication extends SpringBootServletInitializer{
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TeamProjectApplication.class);
-	}
+public class TeamProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeamProjectApplication.class, args);
